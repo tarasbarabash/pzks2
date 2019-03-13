e765afafe8b84eecc2f4e0142ced77080d481a9a
Taras Barabash <tarasbarabash@gmail.com>
2019-03-13 08:37:13 +0200

    Updates

---
+++ precache-manifest.f1b290983288e06bcaa0d8b2877bd41e.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = [
+  {
+    "revision": "a1a2d01bcd034270a3bc92176edfb66c",
+    "url": "/pzks2/static/media/addNodeIcon.a1a2d01b.png"
+  },
+  {
+    "revision": "d7c71afa1e81197a62b4",
+    "url": "/pzks2/static/css/main.4a51aa80.chunk.css"
+  },
+  {
+    "revision": "f75fa258c62f41cc1a3a",
+    "url": "/pzks2/static/js/runtime~main.f75fa258.js"
+  },
+  {
+    "revision": "dd0baa69a69937600dc3bf035f3873c4",
+    "url": "/pzks2/static/media/backIcon.dd0baa69.png"
+  },
+  {
+    "revision": "1fa3c5a032961fb3bb98",
+    "url": "/pzks2/static/js/2.1fa3c5a0.chunk.js"
+  },
+  {
+    "revision": "260c9c6535065674bb5e59a73f894abb",
+    "url": "/pzks2/static/media/cross.260c9c65.png"
+  },
+  {
+    "revision": "d7c71afa1e81197a62b4",
+    "url": "/pzks2/static/js/main.d7c71afa.chunk.js"
+  },
+  {
+    "revision": "d5422321c4f6feed4081891051f9a6b2",
+    "url": "/pzks2/static/media/editIcon.d5422321.png"
+  },
+  {
+    "revision": "d5267b8db2498e44bd567bd12e07576b",
+    "url": "/pzks2/static/media/connectIcon.d5267b8d.png"
+  },
+  {
+    "revision": "02d321edc6e03aea1675001899d8f479",
+    "url": "/pzks2/static/media/deleteIcon.02d321ed.png"
+  },
+  {
+    "revision": "1fa3c5a032961fb3bb98",
+    "url": "/pzks2/static/css/2.dd6c433c.chunk.css"
+  },
+  {
+    "revision": "31b0238e0611797efae5d9dde34b8b33",
+    "url": "/pzks2/index.html"
+  }
+];
