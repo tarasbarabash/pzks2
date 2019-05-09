0f1b29fff239d5d156e7f04ee09a9e5137a791ab
Taras Barabash <tarasbarabash@gmail.com>
2019-05-09 21:23:41 +0300

    Updates

---
+++ precache-manifest.9cc7686d5f5bca7ec835169bdb5d2190.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = [
+  {
+    "revision": "dd0baa69a69937600dc3bf035f3873c4",
+    "url": "/pzks2/static/media/backIcon.dd0baa69.png"
+  },
+  {
+    "revision": "73ce5e495250fb3b0520",
+    "url": "/pzks2/static/css/main.1cdae654.chunk.css"
+  },
+  {
+    "revision": "f75fa258c62f41cc1a3a",
+    "url": "/pzks2/static/js/runtime~main.f75fa258.js"
+  },
+  {
+    "revision": "d5267b8db2498e44bd567bd12e07576b",
+    "url": "/pzks2/static/media/connectIcon.d5267b8d.png"
+  },
+  {
+    "revision": "61f31bdec874b2eb367d",
+    "url": "/pzks2/static/js/2.61f31bde.chunk.js"
+  },
+  {
+    "revision": "a1a2d01bcd034270a3bc92176edfb66c",
+    "url": "/pzks2/static/media/addNodeIcon.a1a2d01b.png"
+  },
+  {
+    "revision": "73ce5e495250fb3b0520",
+    "url": "/pzks2/static/js/main.73ce5e49.chunk.js"
+  },
+  {
+    "revision": "d5422321c4f6feed4081891051f9a6b2",
+    "url": "/pzks2/static/media/editIcon.d5422321.png"
+  },
+  {
+    "revision": "260c9c6535065674bb5e59a73f894abb",
+    "url": "/pzks2/static/media/cross.260c9c65.png"
+  },
+  {
+    "revision": "02d321edc6e03aea1675001899d8f479",
+    "url": "/pzks2/static/media/deleteIcon.02d321ed.png"
+  },
+  {
+    "revision": "61f31bdec874b2eb367d",
+    "url": "/pzks2/static/css/2.beec776a.chunk.css"
+  },
+  {
+    "revision": "3c83307c1c15f768b020ed4b6545eb86",
+    "url": "/pzks2/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/pzks2/precache-manifest.e5650bb0d58a23304532d9410b5e5454.js"
+  "/pzks2/precache-manifest.9cc7686d5f5bca7ec835169bdb5d2190.js"
 );
 
 workbox.clientsClaim();
